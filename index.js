3f1704089c8db0b6c31cbb7ce01cf8ba6b08a6ab
edwinsplit <edwinsplit@freedom.nl>
2026-05-02 23:40:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -114,5 +114,7 @@ app.get("/api/live", async (req, res) => {
 // ---------------------------
 // SERVER STARTEN
 // ---------------------------
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Nivo backend running on port", PORT));
+app.listen(PORT, "0.0.0.0", () => console.log("Nivo backend is running on port", PORT));
+
